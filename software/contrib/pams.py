34fe7d9ff787c5d607a41a3771fdd53105ebccb1
Chris I-B <c.iverachbrereton@gmail.com>
2023-03-25 22:20:13 -0400

    More work implementing the menus. TODO: handle long-presses to change the active item

--- software/contrib/pams.py
+++ software/contrib/pams.py
@@ -40,11 +40,11 @@ QSCALES = {
     #                        C      C#     D      D#     E      F      F#     G      G#     A      A#     B
     "Chromatic" : Quantizer([True,  True,  True,  True,  True,  True,  True,  True,  True,  True,  True,  True]),
     
-    "NatMaj"    : Quantizer([True,  False, True,  False, True,  True,  False, True,  False, True,  False, True]),
-    "HarMaj"    : Quantizer([True,  False, True,  False, True,  True,  False, True,  True,  False, True,  False]),
+    "Nat Maj"   : Quantizer([True,  False, True,  False, True,  True,  False, True,  False, True,  False, True]),
+    "Har Maj"   : Quantizer([True,  False, True,  False, True,  True,  False, True,  True,  False, True,  False]),
     
-    "NatMin"    : Quantizer([True,  False, True,  True,  False, True,  False, True,  True,  False, True,  False]),
-    "HarMin"    : Quantizer([True,  False, True,  True,  False, True,  False, True,  True,  False, False, True]),
+    "Nat Min"   : Quantizer([True,  False, True,  True,  False, True,  False, True,  True,  False, True,  False]),
+    "Har Min"   : Quantizer([True,  False, True,  True,  False, True,  False, True,  True,  False, False, True]),
     
     "Whole"     : Quantizer([True,  False, True,  False, True,  False, True,  False, True,  False, True,  False])
     
@@ -53,8 +53,65 @@ QSCALES = {
     # Or some jazz scales?
 }
 
+QUANTIZERS = [
+    "None",
+    "Chromatic",
+    "Nat Maj",
+    "Har Maj",
+    "Nat Min",
+    "Har Min",
+    "Whole"
+]
+
 ## How many ms does a button need to be held to qualify as a long press?
 LONG_PRESS_MS = 500
+
+CLOCK_MODS = [
+    "x8",
+    "x6",
+    "x4",
+    "x3",
+    "x2",
+    "x1",
+    "/2",
+    "/3",
+    "/4",
+    "/6",
+    "/8",
+    "/12",
+    "/16",
+    "/24",
+    "/32"
+]
+
+CLOCK_RATIOS = {
+    "x8" : 8.0,
+    "x6" : 6.0,
+    "x4" : 4.0,
+    "x3" : 3.0,
+    "x2" : 2.0,
+    "x1" : 1.0,
+    "/2" : 1/2.0,
+    "/3" : 1/3.0,
+    "/4" : 1/4.0,
+    "/6" : 1/6.0,
+    "/8" : 1/8.0,
+    "/12" : 1/12.0,
+    "/16" : 1/16.0,
+    "/24" : 1/24.0,
+    "/32" : 1/32.0
+}
+
+WAVE_SQUARE = 0
+WAVE_RANDOM = 1
+WAVE_SHAPES = [
+    "Square",
+    "Random"
+]
+WAVE_SHAPE_IDS= {
+    "Square": WAVE_SQUARE,
+    "Random": WAVE_RANDOM
+}
 
 class MenuEditableObject:
     """An object that can be dynamically edited via the UI menu
@@ -157,9 +214,6 @@ class PamsOutput(MenuEditableObject):
     """Controls a single output jack
     """
     
-    WAVE_SQUARE = 0
-    WAVE_RANDOM = 1
-    
     def __init__(self, cv_out):
         """Create a new output to control a single cv output
         
@@ -170,9 +224,8 @@ class PamsOutput(MenuEditableObject):
         ## What quantization are we using?
         #
         #  See contrib.pams.QSCALES
-        #
-        #  Disabled when negative
-        self.quantizer_index = -1
+        self.quantizer_key = "None"
+        self.quantizer = None
         
         ## The clock modifier for this channel
         #
@@ -180,11 +233,16 @@ class PamsOutput(MenuEditableObject):
         #  - <1.0 will tick slower than the BPM (e.g. 0.5 will tick once every 2 beats)
         #  - >1.0 will tick faster than the BPM (e.g. 3.0 will tick 3 times per beat)
         self.clock_mod = 1.0
+        self.clock_mod_txt = "x1"
         
         ## What shape of wave are we generating?
         #
         #  For now, stick to square waves for triggers & gates
         self.wave = WAVE_SQUARE
+        self.wave_shape_txt = "Wave"
+        
+        ## The amplitude of the output as a [0, 100] percentage
+        self.amplitude = 50   # default to 5V gates
         
         ## Euclidean -- number of steps in the pattern (0 = disabled)
         self.e_steps = 0
@@ -204,13 +262,14 @@ class PamsOutput(MenuEditableObject):
         """Return a dictionary with all the configurable settings to write to disk
         """
         return {
-            "clock_mod" : self.clock_mod,
+            "clock_mod" : self.clock_mod_txt,
             "e_step"    : self.e_steps,
             "e_trigs"   : self.e_trigs,
             "e_rot"     : self.e_rot,
             "skip"      : self.skip_prob,
-            "wave"      : self.wave,
-            "quant"     : self.quantizer_index
+            "wave"      : self.wave_shape_txt,
+            "amplitude" : self.amplitude,
+            "quant"     : self.quantizer_key
         }
     
     def load_settings(self, settings):
@@ -219,13 +278,21 @@ class PamsOutput(MenuEditableObject):
         @param settings  A dict with the same keys as the one returned by to_dict()
         """
         
-        self.clock_mod = settings["clock_mod"]
+        self.clock_mod_txt = settings["clock_mod"]
+        self.clock_mod = CLOCK_RATIOS[self.clock_mod_txt]
         self.e_step = settings["e_step"]
         self.e_trigs = settings["e_trigs"]
         self.e_rot = settings["e_rot"]
         self.skip = settings["skip"]
-        self.wave = settings["wave"]
-        self.quantizer_index = settings["quant"]
+        self.wave_shape_txt = settings["wave"]
+        self.wave = WAVE_SHAPE_IDS[self.wave_shape_txt]
+        self.amplitude = settings["amplitude"]
+        self.quantizer_key = settings["quant"]
+        
+        if self.quantizer_key in QSCALES.keys():
+            self.quantizer = QSCALES[self.quantizer_key]
+        else:
+            self.quantizer = None
         
         self.recalculate_pattern()
         
@@ -233,6 +300,23 @@ class PamsOutput(MenuEditableObject):
         """If we've changed anything via the menu, recalculate the necessary changes
         """
         super().apply_changes()
+        
+        # look up the numerical clock mod from the text
+        self.clock_mod = CLOCK_RATIOS[self.clock_mod_txt]
+        
+        # make sure the euclidean limits are valid
+        # the rotation & number of triggers must always be <= the number of steps!
+        if self.e_rot > self.e_steps:
+            self.e_rot = 0
+        if self.e_trigs > self.e_steps:
+            self.e_trigs = self.e_steps
+            
+        # Choose the correct quantizer
+        if self.quantizer_key in QSCALES.keys():
+            self.quantizer = QSCALES[self.quantizer_key]
+        else:
+            self.quantizer = None
+        
         self.__recalculate_pattern()
         
     def __recalculate_pattern(self):
@@ -256,34 +340,35 @@ class PamsOutput(MenuEditableObject):
 class SettingChooser:
     """Menu UI element for displaying an option and the choices associated with it
     """
-    def __init__(self):
-        self.title = ""
-        self.options = []
-        self.value = None
-        self.dest_obj = None
-        self.dest_prop = None
+    def __init__(self, title, options, dest_obj, dest_prop):
+        """Create a setting chooser for a given item
         
-        self.is_writable = False
-        
-    def reconfigure(self, title, current_value, available_values, dest_obj, dest_prop):
-        """Reconfigure the UI to display a particular setting
-
         We use dest_obj.__setattr__(dest_prop, X) to set the new value. If this was C or C++
         we could use a pointer, but that's not an option here so we're using a Pythony
         work-around.
 
         @param title  The title of this menu item
-        @param current_value  The current value of the option
         @param available_values  A list of available options to choose from
         @param dest_obj  The object whose property we're editing, e.g. a MasterClock instance
         @param dest_prop  The name of the attribute of dest_obj to edit.
         """
-        
         self.title = title
-        self.value = current_value
-        self.options = available_values
-        self.dest_obj = dest_obj
-        self.dest_prop = dest_prop
+        self.options = options
+        self.dest_obj = None
+        self.dest_prop = None
+        
+        self.is_writable = False
+        
+    def reconfigure_options(self, options):
+        """Reconfigure the the available options.
+
+        For example, if we change the number of steps in a Euclidean rhythm, the number of
+        pulses cannot exceed the number of steps
+        
+        @param options  The new set of options we allow
+        """
+        
+        self.options = options
         
     def set_editable(self, can_edit):
         """Set whether or not we can write to this setting
@@ -323,6 +408,85 @@ class SettingChooser:
         self.dest_obj.apply_changes()
         
 
+class Menu:
+    """Generic menu object
+    """
+    def __init__(self):
+        self.items = []
+        
+        self.active_item = None
+        
+    def draw(self):
+        if self.active_item:
+            self.active_item.draw()
+        else:
+            oled.fill(0)
+            oled.show()
+            
+    def on_long_press():
+        if self.active_item:
+            self.active_item.on_long_press()
+
+class TopLevelMenu(Menu):
+    def __init__(self, script):
+        """Create the top-level menu for the application
+
+        @param script  The PamsWorkout object the meny belongs to
+        """
+        Menu.__init__(self)
+        
+        self.pams_workout = script
+        
+        self.submenus = [
+            ClockMenu(self, script.clock)
+        ]
+        for i in range(len(script.channels)):
+            self.submenus.append(ChannelMenu(self, script.channels[i]))
+        
+        self.items = [
+            SettingChooser("BPM", list(range(MasterClock.MIN_BPM, MasterClock.MAX_BPM+1)), script.clock, "bpm"),
+            SettingChooser("CV1 Mod", CLOCK_MODS, script.channels[0], "clock_mod_txt"),
+            SettingChooser("CV2 Mod", CLOCK_MODS, script.channels[1], "clock_mod_txt"),
+            SettingChooser("CV3 Mod", CLOCK_MODS, script.channels[2], "clock_mod_txt"),
+            SettingChooser("CV4 Mod", CLOCK_MODS, script.channels[3], "clock_mod_txt"),
+            SettingChooser("CV5 Mod", CLOCK_MODS, script.channels[4], "clock_mod_txt"),
+            SettingChooser("CV6 Mod", CLOCK_MODS, script.channels[5], "clock_mod_txt")
+        ]
+        
+        self.active_item = k1.choice(self.items)
+        
+class Submenu(Menu):
+    def __init__(self, parent):
+        Menu.__init__(self)
+        self.parent = parent
+        self.parent.child = self
+        
+class ClockMenu(Submenu):
+    """Submenu for the main clock
+    """
+    def __init__(self, parent, clock):
+        Submenu.__init__(self, parent)
+        self.clock = clock
+        
+        self.items.append(
+            SettingChooser("Reset", [True, False], clock, "reset_on_start")
+        )
+    
+class ChannelMenu(Submenu):
+    """Submenu for a single CV channel
+    """
+    def __init__(self, parent, channel):
+        Submenu.__init__(self, parent)
+        self.channel = channel
+        
+        self.items.append(SettingChooser("Wave", WAVE_SHAPES, channel, "wave_shape_txt"))
+        self.items.append(SettingChooser("Amplitude", list(range(0, 101)), channel, "amplitude"))
+        self.items.append(SettingChooser("Skip %", list(range(0, 101)), channel, "skip_prob"))
+        self.items.append(SettingChooser("Eucl. Steps", list(range(0, 33)), channel, "e_steps"))
+        self.items.append(SettingChooser("Eucl. Pulses", list(range(0, 33)), channel, "e_trigs"))
+        self.items.append(SettingChooser("Eucl. Rot.", list(range(0, 33)), channel, "e_rot"))
+        self.items.append(SettingChooser("Quant.", QUANTIZERS, channel, "quantizer_key"))
+
 class PamsWorkout(EuroPiScript):
     """The main script for the Pam's Workout implementation
     """
@@ -338,6 +502,8 @@ class PamsWorkout(EuroPiScript):
             PamsOutput(cv6),
         ]
         self.clock = MasterClock(120, self.channels)
+        
+        self.menu = TopLevelMenu(self)
         
         @b1.handler
         def on_b1_press():
