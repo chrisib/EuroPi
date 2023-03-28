af92008b4e0c5f3116d5974ce8f8af720f1382c1
Chris I-B <c.iverachbrereton@gmail.com>
2023-03-28 01:45:48 -0400

    Sort the bootloader scripts & class keys to ensure the lists are in the same order when comparing them

--- software/tests/contrib/test_menu.py
+++ software/tests/contrib/test_menu.py
@@ -16,4 +16,8 @@ def test_menu_imports(mock_time_module):
     """User the bootloader code to test that every script declared in EUROPI_SCRIPTS can be imported."""
     bootloader = BootloaderMenu(EUROPI_SCRIPTS)
     classes = bootloader.load_script_classes(bootloader.scripts)
-    assert EUROPI_SCRIPTS == list(classes.keys()), "Some EUROPI_SCRIPTs were not able to be loaded."
+    classes = list(classes.keys())
+    classes.sort()
+    scripts = list(EUROPI_SCRIPTS)
+    scripts.sort()
+    assert classes == scripts, "Some EUROPI_SCRIPTs were not able to be loaded."
