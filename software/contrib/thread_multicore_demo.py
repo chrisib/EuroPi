68688a5e27c4aaff3cecb88b1c44745db1e3a54f
Chris I-B <ve4cib@gmail.com>
2024-02-18 15:52:59 -0500

    Don't use digial I/O ISRs; poll the button states as part of the main loop

--- software/contrib/thread_multicore_demo.py
+++ software/contrib/thread_multicore_demo.py
@@ -10,30 +10,63 @@ from europi import b1, b2, oled
 
 GC_INTERVAL_MS = 250
 
+class DigitalInputMonitor:
+    """Polls the states of the digital inputs and sets bools to indicate if rising/falling edges are detected
+    """
+    def __init__(self):
+        self.din_rising = False
+        self.din_falling = False
+        self.b1_rising = False
+        self.b1_falling = False
+        self.b2_rising = False
+        self.b2_falling = False
+
+        self.din_high = False
+        self.b1_high = False
+        self.b2_high = False
+
+    def check(self):
+        din_state = europi.din.value() != 0
+        b1_state = europi.b1.value() != 0
+        b2_state = europi.b2.value() != 0
+
+        self.din_rising = not self.din_high and din_state
+        self.din_falling = self.din_high and not din_state
+
+        self.b1_rising = not self.b1_high and b1_state
+        self.b1_falling = self.b1_high and not b1_state
+
+        self.b2_rising = not self.b2_high and b2_state
+        self.b2_falling = self.b2_high and not b2_state
+
+        self.din_high = din_state
+        self.b1_high = b1_state
+        self.b2_high = b2_state
+
+
 class MulticoreDemoScript(europi_script.EuroPiScript):
     def __init__(self):
         super().__init__()
 
         self.b1_time = time.ticks_ms()
         self.b2_time = time.ticks_ms()
-
         self.lfo_tick = 0
-
-        @b1.handler
-        def on_b1_press():
-            self.b1_time = time.ticks_ms()
-
-        @b2.handler
-        def on_b2_press():
-            self.b2_time = time.ticks_ms()
 
     def core0_thread(self):
         """Update the OLED and handle ISRs on the primary core
 
         This is technically the main loop of the program
         """
+        io_state = DigitalInputMonitor()
         last_gc_at = time.ticks_ms()
         while True:
+            io_state.check()
+
+            if io_state.b1_rising:
+                self.b1_time = time.ticks_ms()
+            if io_state.b2_rising:
+                self.b2_time = time.ticks_ms()
+
             oled.centre_text(f"{self.b1_time}\n{self.b2_time}\n{self.lfo_tick}")
 
             # do garbage collection periodically
